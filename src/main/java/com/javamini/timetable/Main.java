6379f3763d610883906458c78641e884bcbd295b
probs0809 <prabodhmayekar@gmail.com>
2019-10-30 19:29:24 +0530

    Addition of GUI

--- src/main/java/com/javamini/timetable/Main.java
+++ src/main/java/com/javamini/timetable/Main.java
@@ -5,6 +5,8 @@
  */
 package com.javamini.timetable;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.*;
 
 /**
@@ -13,35 +15,62 @@ import java.util.*;
  */
 public class Main {
     public static void main(String[] args) {
-        inputdata id = new inputdata();
-        id.takeinput();
-        inputdata.hoursperday = 7;
-        inputdata.daysperweek = 5;
-        Utility.cli = true;
-        //Utility.printInputData();
-        new TimeTable();
-       //s Utility.printSlots();
-        ArrayList<String> day = new ArrayList<>();
-        day.add("Monday");
-        day.add("Tuesday");
-        day.add("Wenesday");
-        day.add("Thusday");
-        day.add("Friday");
-        day.add("Saturday");
-        day.add("Sunday");
-
-        ArrayList<String> hour = new ArrayList<>();
-        hour.add("08:00 PM");
-        hour.add("09:00 AM");
-        hour.add("10:00 AM");
-        hour.add("11:00 AM");
-        hour.add("12:00 PM");
-        hour.add("01:00 PM");
-        hour.add("02:00 PM");
-        hour.add("03:00 PM");
-        hour.add("04:00 PM");
-        hour.add("05:00 PM");
-        Utility.generateTimeTable(5,day,hour);
+//        inputdata id = new inputdata();
+//        id.takeinput();
+//        inputdata.hoursperday = 7;
+//        inputdata.daysperweek = 5;
+//        Utility.cli = true;
+//        //Utility.printInputData();
+//        new TimeTable();
+//       //s Utility.printSlots();
+//        ArrayList<String> day = new ArrayList<>();
+//        day.add("Monday");
+//        day.add("Tuesday");
+//        day.add("Wenesday");
+//        day.add("Thusday");
+//        day.add("Friday");
+//        day.add("Saturday");
+//        day.add("Sunday");
+//
+//        ArrayList<String> hour = new ArrayList<>();
+//        hour.add("08:00 PM");
+//        hour.add("09:00 AM");
+//        hour.add("10:00 AM");
+//        hour.add("11:00 AM");
+//        hour.add("12:00 PM");
+//        hour.add("01:00 PM");
+//        hour.add("02:00 PM");
+//        hour.add("03:00 PM");
+//        hour.add("04:00 PM");
+//        hour.add("05:00 PM");
+//        Utility.generateTimeTable(5,day,hour);
+        SplashScreen splash = new SplashScreen();
+        MainMenu mainMenu = new MainMenu();
+        mainMenu.man_group.setOpaque(true);
+        mainMenu.man_sub.setOpaque(true);
+        mainMenu.man_teacher.setOpaque(true);
+        mainMenu.man_tt.setOpaque(true);
+        splash.setVisible(true);
+        try {
+            
+            for (int i = 0; i < 100; i++) {
+                Thread.sleep(100);
+                splash.progressBar.setValue(i);
+                
+            }
+            splash.setVisible(false);
+            mainMenu.setVisible(true);
+            
+        } catch (Exception e) {
+        }
+        
+        mainMenu.man_group.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+        });
+                  
         
     }
 }
